ba2b8b1cb48f3944276dc86c16d771e1f3f7ae04
Madjar Evan <evanmadjar@gmail.com>
2024-06-04 17:08:17 +0300

    feat: Implement interfaces for our auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
@@ -10,5 +11,8 @@ app.get('/', (req, res) => {
   // root route
   res.send('Hello World!!')
 })
+
+
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
