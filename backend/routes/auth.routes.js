5cbc849cfa34b3796df6dfb971c5163cc1aa5042
Madjar Evan <evanmadjar@gmail.com>
2024-06-04 17:42:31 +0300

    chore: Add slash to the ends of the routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -8,10 +8,10 @@ import {
 
 const router = express.Router()
 
-router.get('/signup', signup)
+router.post('/signup/', signup)
 
-router.get('/login', login)
+router.post('/login/', login)
 
-router.get('/logout', logout)
+router.post('/logout/', logout)
 
 export default router
