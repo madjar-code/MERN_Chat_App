16575923dbec0b65cd8c6b2ec35d9a45abddf2a8
Madjar Evan <evanmadjar@gmail.com>
2024-06-04 17:43:03 +0300

    feat: Add connections to MongoDB functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import connectToMongoDB from "./db/connectToMongoDB.js"
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
@@ -12,7 +13,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!!')
 })
 
-
 app.use('/api/auth', authRoutes)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`)
+})
