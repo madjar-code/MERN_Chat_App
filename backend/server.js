83dc612339215d4a3fa6aec0de139ca3d337e63d
Madjar Evan <evanmadjar@gmail.com>
2024-06-05 10:53:42 +0300

    fix: Add JSON handling ability

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,18 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
 import connectToMongoDB from "./db/connectToMongoDB.js"
 
-dotenv.config()
+const app = express()
 const PORT = process.env.PORT || 5000
 
-const app = express()
+dotenv.config()
 
-app.get('/', (req, res) => {
-  // root route
-  res.send('Hello World!!')
-})
-
+app.use(express.json())
 app.use('/api/auth', authRoutes)
+
+// app.get('/', (req, res) => {
+//   // root route
+//   res.send('Hello World!!')
+// })
 
 app.listen(PORT, () => {
   connectToMongoDB();
