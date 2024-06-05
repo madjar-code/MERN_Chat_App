bc53b16df0a499d7745964120ab8552bd88edfb8
Madjar Evan <evanmadjar@gmail.com>
2024-06-05 11:23:08 +0300

    feat: Implement login functionality

--- backend/controlles/auth.controller.js
+++ backend/controlles/auth.controller.js
@@ -1,3 +1,4 @@
+
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import generateTokenAndSetCookie from '../utils/generateToken.js'
@@ -61,8 +62,26 @@ export const signup = async (req, res) => {
   }
 }
 
-export const login = (req, res) => {
-  console.log('loginUser')
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const user = await User.findOne({ username })
+    const isPasswordCorrect = await bcryptjs.compare(password, user?.password || "")
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({error: "Invalid username or password"})
+    }
+    generateTokenAndSetCookie(user._id, res)
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    })
+  } catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
 }
 
 export const logout = (req, res) => {
