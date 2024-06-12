eca0809123bf48bb585ed7152bb74ba4d6f3cfd1
Madjar Evan <evanmadjar@gmail.com>
2024-06-12 14:11:30 +0300

    feat: Finish doing authentication functionality

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -5,9 +5,9 @@ const Message = () => {
         <div className='w-10 rounded-full'>
           <img
             alt='Tailwind CSS chat bubble component'
-            src={
-              'https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profule_user_contact_person_avatar_placeholer-512.png'
-            }
+            // src={
+            //   'https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholer-512.png'
+            // }
           />
         </div>
       </div>
